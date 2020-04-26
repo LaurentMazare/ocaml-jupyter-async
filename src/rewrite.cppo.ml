91b5513740abf94c3d093ad5e2e73b91e7db4b6d
laurent <laurent.mazare@gmail.com>
2020-04-26 19:45:28 +0100

    Avoid some warning.

--- src/rewrite.cppo.ml
+++ src/rewrite.cppo.ml
@@ -1,6 +1,7 @@
 let apply_ppx ~sourcefile phrase =
   match phrase with
   | Parsetree.Ptop_def ptop_def ->
+    let _ = sourcefile in
     let ptop_def =
       Pparse.apply_rewriters_str ptop_def ~restore:true ~tool_name:"ocaml"
 #if OCAML_VERSION < (4,09,0)
