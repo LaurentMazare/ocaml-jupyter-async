ef9e91edffbb28f80d4d9088d6cfeb6faffe639d
laurent <laurent.mazare@gmail.com>
2020-04-26 09:02:03 +0100

    More completion code.

--- src/worker.ml
+++ src/worker.ml
@@ -5,8 +5,15 @@ open! Core
 
 let worker_buf_len = 4096
 
-type worker_input = { code : string } [@@deriving bin_io]
-type worker_output = unit Or_error.t [@@deriving bin_io]
+type worker_input =
+  | Execute of { code : string }
+  | Complete of { code : string }
+[@@deriving bin_io]
+
+type worker_output =
+  | Execute_output of unit Or_error.t
+  | Complete_output of (int * (string * string) list)
+[@@deriving bin_io]
 
 type t =
   { pid : Pid.t
@@ -14,6 +21,7 @@ type t =
   ; stdout_reader : Async.Reader.t
   ; stderr_reader : Async.Reader.t
   ; writer : Async.Writer.t
+  ; sequencer : unit Async.Sequencer.t
   }
 
 let worker ~worker_in_read ~worker_out_write =
@@ -23,8 +31,7 @@ let worker ~worker_in_read ~worker_out_write =
   in
   let in_channel = Unix.in_channel_of_descr worker_in_read in
   let out_channel = Unix.out_channel_of_descr worker_out_write in
-  let on_input { code } =
-    let result = Ocaml_toploop.toploop_eval code ~verbose:true in
+  let send_output result =
     (* It is quite inefficient to re-allocate the bigstring but this should be small
        enough not to be a problem. *)
     let bigstring =
@@ -35,6 +42,17 @@ let worker ~worker_in_read ~worker_out_write =
     let result = Bigstring.to_string bigstring in
     Out_channel.output_string out_channel result;
     Out_channel.flush out_channel
+  in
+  let on_input input =
+    let result =
+      match input with
+      | Execute { code } ->
+        let result = Ocaml_toploop.toploop_eval code ~verbose:true in
+        Complete.reset ();
+        Execute_output result
+      | Complete { code } -> Complete_output (Complete.complete code)
+    in
+    send_output result
   in
   let buf = Bytes.create worker_buf_len in
   let rec loop () =
@@ -72,15 +90,36 @@ let spawn () =
     let writer =
       Fd.create Char worker_in_write (Info.of_string "worker-in-write") |> Writer.create
     in
-    { pid; reader; writer; stdout_reader; stderr_reader }
+    { pid
+    ; reader
+    ; writer
+    ; stdout_reader
+    ; stderr_reader
+    ; sequencer = Async.Sequencer.create ()
+    }
 
 let execute t ~code =
   let open Async in
-  Writer.write_bin_prot t.writer bin_writer_worker_input { code };
-  let%map result = Reader.read_bin_prot t.reader bin_reader_worker_output in
-  match result with
-  | `Ok result -> result
-  | `Eof -> failwith "unexpected end of file from worker"
+  Throttle.enqueue t.sequencer (fun () ->
+      Writer.write_bin_prot t.writer bin_writer_worker_input (Execute { code });
+      let%map result = Reader.read_bin_prot t.reader bin_reader_worker_output in
+      match result with
+      | `Ok (Execute_output result) -> result
+      | `Ok (Complete_output _) -> assert false
+      | `Eof -> failwith "unexpected end of file from worker")
+
+let complete t ~code =
+  let open Async in
+  if Async.Throttle.num_jobs_running t.sequencer = 0
+  then
+    Throttle.enqueue t.sequencer (fun () ->
+        Writer.write_bin_prot t.writer bin_writer_worker_input (Complete { code });
+        let%map result = Reader.read_bin_prot t.reader bin_reader_worker_output in
+        match result with
+        | `Ok (Complete_output res) -> `ok res
+        | `Ok (Execute_output _) -> assert false
+        | `Eof -> failwith "unexpected end of file from worker")
+  else return `busy
 
 let stdout_reader t = t.stdout_reader
 let stderr_reader t = t.stderr_reader
--- src/worker.mli
+++ src/worker.mli
@@ -5,5 +5,11 @@ type t
 
 val spawn : unit -> t
 val execute : t -> code:string -> unit Or_error.t Deferred.t
+
+val complete
+  :  t
+  -> code:string
+  -> [ `ok of int * (string * string) list | `busy ] Deferred.t
+
 val stdout_reader : t -> Reader.t
 val stderr_reader : t -> Reader.t
