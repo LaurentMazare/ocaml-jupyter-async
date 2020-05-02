53449392ea3081d41f8279c67dbc48c5211e62d7
laurent <laurent.mazare@gmail.com>
2020-05-02 14:00:04 +0100

    Avoid killing async on sigint.

--- src/toploop/kernel_toploop.ml
+++ src/toploop/kernel_toploop.ml
@@ -1,9 +1,14 @@
+open! Core
+
 type t = unit
 
 let create () =
   Ocaml_toploop.maybe_initialize ();
   Complete.reset ();
-  ()
+  Signal.Expert.handle Signal.int (fun _sigint ->
+      if Async.Scheduler.is_running ()
+      then Stdio.eprintf "received sigint, doing nothing as async is running\n%!"
+      else raise Caml.Sys.Break)
 
 let eval () code =
   let result = Ocaml_toploop.toploop_eval code in
